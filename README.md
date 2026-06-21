18e584bd2935a17f486097faa7d2723b81b6af1d
Frantisek Repkovsky <frantisek.repkovsky@gmail.com>
2026-06-21 15:48:46 +0200

    Replace Paramiko with system ssh via subprocess to avoid PyO3/bcrypt incompatibility

--- README.md
+++ README.md
@@ -17,9 +17,8 @@ iDetect is a python plugin for Domoticz, which allows you to detect the presence
 * Getting connected devices from Netgear Orbi using http api (as an example for adding other methods)
 
 ## (New) requirements
-* Some trackers have their own requirements. E.g. using a SSH based tracker requires 'paramiko' to be installed. The FRITZ!Box tracker needs 'fritzconnect'. If these requirements are not met you cannot use these trackers, but other trackers will still work.
+* Some trackers have their own requirements. E.g. The FRITZ!Box tracker needs 'fritzconnect'. If these requirements are not met you cannot use these trackers, but other trackers will still work.
 * Python 3.5 or newer
-* No longer needs sshpass for password based authentication (paramiko takes care of that).
 
 More information and support can be found on the Domoticz forum topic at https://www.domoticz.com/forum/viewtopic.php?f=65&t=20467.  
 
@@ -38,8 +37,8 @@ I think these names better reflect their function, since other devices than rout
 
 ## Installation
 Assumming you have allready installed pip3 and git  
-First install the required (but not standard) python modules if they ar not already installed   
-`sudo pip3 install requests paramiko`  
+First install the required (but not standard) python modules if they are not already installed   
+`sudo pip3 install requests`  
 
 Assuming domoticz is installed in /home/pi (the default on raspberry pi)  
 `cd /home/pi/domoticz/plugins`  
--- trackers/ssh_tracker.py
+++ trackers/ssh_tracker.py
@@ -1,127 +1,178 @@
-import Domoticz
+import os
+import shlex
 import subprocess
-import helpers.data_helper as data_helper
+from datetime import datetime
+
+import Domoticz
 from trackers.tracker_base import tracker
-import helpers.data_helper as data_helper
-from datetime import datetime, timedelta
-from time import sleep
-import paramiko
+
 
 class ssh_tracker(tracker):
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
-		self.trackerscript = ''
-		self.sshbin = 'ssh'
-		self.connected = False
-		self.my_transport = None
-		if self.tracker_port is None:
-			self.tracker_port = 22
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.trackerscript = ''
+        self.sshbin = 'ssh'
+        self.connected = False
+        self.strict_host_key_checking = False
+        self.connect_timeout = 5
 
-		Domoticz.Debug(self.tracker_ip + ' Tracker is of the ssh kind')
-		self.client = paramiko.SSHClient()
-		self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-		self.ssh_connect()
-			
-	def logger(self,message):
-		Domoticz.Log(message)
+        if self.tracker_port is None:
+            self.tracker_port = 22
 
-	def poll_present_tag_ids(self):
-		Domoticz.Debug(self.tracker_ip + ' Start poll and return results to ' + str(self.receiver_callback))
-		success, raw_data = self.getfromssh(self.trackerscript)
-		if not success:
-			self.error_count = self.error_count + 1
-			Domoticz.Log(self.tracker_ip + ' Could not be polled')
-			return
-		else:
-			self.error_count = 0
-			self.receiver_callback(raw_data)
+        Domoticz.Debug(self.tracker_ip + ' Tracker is of the ssh kind')
+        self.ssh_connect()
 
-	def prepare_for_polling(self):
-		#Should be implemented in specfic tracker
-		Domoticz.Debug(self.tracker_ip + ' Has no prepare_for_pollling method.')
-		return True
+    def logger(self, message):
+        Domoticz.Log(message)
 
-	def ssh_connect(self):
-		Domoticz.Debug(self.tracker_ip + ' ====> SSH start connect on port ' + str(self.tracker_port))
-		if self.tracker_password == '':
-			if self.tracker_keyfile != '':
-				my_key_file = self.tracker_keyfile
-				rsa_key = None
-				try:
-					rsa_key=paramiko.RSAKey.from_private_key_file(my_key_file)
-				except Exception as e:
-					Domoticz.Error(self.tracker_ip + ' ====> SSH Could not get RSA from private key. Exception: ' + str(e))
-				Domoticz.Debug(self.tracker_ip + ' ====> SSH using key: ' + str(my_key_file))
-				try:
-					self.client.connect(self.tracker_ip, port=self.tracker_port, username=self.tracker_user, pkey=rsa_key, timeout=5)
-				except Exception as e:
-					Domoticz.Error(self.tracker_ip + ' SSH Could not connect (using custom key file). Exception: ' + str(e))
-					self.connected = False
-					return
-			else:
-				try:
-					self.client.connect(self.tracker_ip, port=self.tracker_port, username=self.tracker_user, timeout=5)
-				except Exception as e:
-					Domoticz.Error(self.tracker_ip + ' SSH Could not connect (with os key). Exception: ' + str(e))
-					self.connected = False
-					return False
-		else:
-			try:
-				self.client.connect(self.tracker_ip, port=self.tracker_port, username=self.tracker_user, password=self.tracker_password, look_for_keys=False, timeout=5)
-			except Exception as e:
-				Domoticz.Error(self.tracker_ip + ' ====> SSH Could not connect (using password). Exception: ' + str(e))
-				self.connected = False
-				return False
-		try:
-			self.my_transport = self.client.get_transport()
-			Domoticz.Status(self.tracker_ip + ' ====> SSH connection established')
-		except:
-			Domoticz.Error(self.tracker_ip + ' ====> SSH connection failed (no transport)')
-			self.connected = False
-			return False
-		self.connected = True
-		return True
+    def poll_present_tag_ids(self):
+        Domoticz.Debug(self.tracker_ip + ' Start poll and return results to ' + str(self.receiver_callback))
+        success, raw_data = self.getfromssh(self.trackerscript)
+        if not success:
+            self.error_count = self.error_count + 1
+            Domoticz.Log(self.tracker_ip + ' Could not be polled')
+            return
 
-	def getfromssh(self, tracker_cli, alltimeout=5, sshtimeout=3):
-		Domoticz.Debug(self.tracker_ip + " ====> SSH Fetching data using: " + tracker_cli)
-		starttime=datetime.now()
-		if not self.connected:
-			Domoticz.Debug(self.tracker_ip + ' ====> SSH not connected ... connecting')
-			self.ssh_connect()
-			return False, ''
-		try:
-			stdin, stdout, stderr = self.client.exec_command(tracker_cli, timeout=5)
-			ssh_output = stdout.read().decode("utf-8")
-			ssh_error = stderr.read().decode("utf-8")
-			if ssh_error != '':
-				Domoticz.Error(self.tracker_ip + ' ====> SSH returned error:' + ssh_error)
-#			if ssh_output == '':
-#				Domoticz.Error(self.tracker_ip + ' ====> SSH returned empty response. Transport active: ' + str(self.my_transport.is_active()))
-			Domoticz.Debug(self.tracker_ip + ' ====> SSH returned (decoded):' + ssh_output)
-		except Exception as e:
-			Domoticz.Error(self.tracker_ip + ' ====> SSH failed with exception: ' + str(e))
-			Domoticz.Debug(self.tracker_ip + " ====> SSH tried for " + str((datetime.now()-starttime).microseconds//1000) + " milliseconds.")
-			try:
-				self.client.close()
-				Domoticz.Status(self.tracker_ip + ' ====> SSH resetting connection')
-			except:
-				Domoticz.Debug(self.tracker_ip + ' ====> SSH connection reset failed')
-			self.connected = False
-			return False, ''
-		timespend=datetime.now()-starttime
-		Domoticz.Debug(self.tracker_ip + " ====> SSH session took " + str(timespend.microseconds//1000) + " milliseconds.")
-		return True, ssh_output
+        self.error_count = 0
+        self.receiver_callback(raw_data)
 
-	def stop_now(self):
-		self.is_ready = False
-		try:
-			self.client.close()
-			Domoticz.Debug(self.tracker_ip + ' ====> SSH client closed')
-		except:
-			Domoticz.Debug(self.tracker_ip + ' ====> Unable to close SSH session')
-		if self.my_transport is None:
-			Domoticz.Debug(self.tracker_ip + ' ====> SSH no transport')
-		else:
-			Domoticz.Debug(self.tracker_ip + ' ====> SSH wait for transport to close')
-			self.my_transport.join()
-		super().stop_now()
+    def prepare_for_polling(self):
+        # Should be implemented in specific tracker
+        Domoticz.Debug(self.tracker_ip + ' Has no prepare_for_pollling method.')
+        return True
+
+    def _known_hosts_target(self):
+        # Keep behavior close to Paramiko AutoAddPolicy() by disabling host key
+        # verification when strict checking is not requested.
+        return 'NUL' if os.name == 'nt' else '/dev/null'
+
+    def build_ssh_command(self, tracker_cli, sshtimeout=5):
+        target = self.tracker_ip
+        if self.tracker_user:
+            target = self.tracker_user + '@' + self.tracker_ip
+
+        ssh_cmd = [
+            self.sshbin,
+            '-o', 'BatchMode=yes',
+            '-o', 'ConnectTimeout=' + str(sshtimeout),
+            '-p', str(self.tracker_port),
+        ]
+
+        if not self.strict_host_key_checking:
+            ssh_cmd.extend([
+                '-o', 'StrictHostKeyChecking=accept-new',
+            ])
+
+        if self.tracker_keyfile:
+            ssh_cmd.extend(['-i', self.tracker_keyfile])
+
+        ssh_cmd.append(target)
+        ssh_cmd.append(tracker_cli)
+        return ssh_cmd
+
+    def ssh_connect(self):
+        Domoticz.Debug(self.tracker_ip + ' ====> SSH start connect on port ' + str(self.tracker_port))
+
+        # The subprocess/OpenSSH backend intentionally supports only non-interactive
+        # authentication methods. Password-based authentication would either block
+        # on a prompt or require an unsafe helper such as sshpass.
+        if self.tracker_password:
+            Domoticz.Error(
+                self.tracker_ip
+                + ' ====> SSH password authentication is not supported by subprocess backend'
+            )
+            self.connected = False
+            return False
+
+        if self.tracker_keyfile:
+            Domoticz.Debug(self.tracker_ip + ' ====> SSH using key: ' + str(self.tracker_keyfile))
+        else:
+            Domoticz.Debug(
+                self.tracker_ip + ' ====> SSH using OS-level key/default ssh configuration'
+            )
+
+        # No persistent SSH session is established here. A new ssh process is started
+        # for each poll. Mark configuration as ready.
+        Domoticz.Status(self.tracker_ip + ' ====> SSH subprocess backend ready')
+        self.connected = True
+        return True
+
+    def getfromssh(self, tracker_cli, alltimeout=5, sshtimeout=3):
+        Domoticz.Debug(self.tracker_ip + ' ====> SSH Fetching data using: ' + tracker_cli)
+        starttime = datetime.now()
+
+        if not self.connected:
+            Domoticz.Debug(self.tracker_ip + ' ====> SSH not connected ... connecting')
+            if not self.ssh_connect():
+                return False, ''
+
+        ssh_cmd = self.build_ssh_command(tracker_cli, sshtimeout)
+
+        try:
+            Domoticz.Debug(
+                self.tracker_ip
+                + ' ====> SSH command: '
+                + ' '.join(shlex.quote(arg) for arg in ssh_cmd)
+            )
+
+            proc = subprocess.run(
+                ssh_cmd,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                universal_newlines=True,
+                timeout=alltimeout,
+                check=False,
+            )
+
+            ssh_output = proc.stdout
+            ssh_error = proc.stderr
+
+            if proc.returncode != 0:
+                Domoticz.Error(
+                    self.tracker_ip
+                    + ' ====> SSH returned rc '
+                    + str(proc.returncode)
+                    + ': '
+                    + ssh_error.strip()
+                )
+                self.connected = False
+                return False, ''
+
+            if ssh_error:
+                Domoticz.Error(self.tracker_ip + ' ====> SSH returned error:' + ssh_error)
+
+            Domoticz.Debug(self.tracker_ip + ' ====> SSH returned (decoded):' + ssh_output)
+
+        except subprocess.TimeoutExpired:
+            Domoticz.Error(
+                self.tracker_ip + ' ====> SSH failed with exception: command timed out'
+            )
+            self.connected = False
+            return False, ''
+        except FileNotFoundError:
+            Domoticz.Error(
+                self.tracker_ip
+                + ' ====> SSH failed with exception: ssh binary not found: '
+                + str(self.sshbin)
+            )
+            self.connected = False
+            return False, ''
+        except Exception as e:
+            Domoticz.Error(self.tracker_ip + ' ====> SSH failed with exception: ' + str(e))
+            self.connected = False
+            return False, ''
+
+        timespend = datetime.now() - starttime
+        Domoticz.Debug(
+            self.tracker_ip
+            + ' ====> SSH session took '
+            + str(timespend.microseconds // 1000)
+            + ' milliseconds.'
+        )
+        return True, ssh_output
+
+    def stop_now(self):
+        self.is_ready = False
+        self.connected = False
+        Domoticz.Debug(self.tracker_ip + ' ====> SSH subprocess backend stopped')
+        super().stop_now()
